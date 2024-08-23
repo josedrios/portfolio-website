596ea8b843589c18463bd12cae2d8cdca8915310
Jose Rios <joserios0531@gmail.com>
2024-08-23 15:25:32 -0700

    Scrapping original idea, was quiet bland and not stimulating at all

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,12 @@
 import * as React from 'react';
-import './styles.scss'
-import bigScreenImage from './images/squarescreen.png'
+import './styles/index.scss'
 
 function App() {
   return (
-    <div id='app-container'>
-      <NameHeader/>
-      <div id="body-container">
-        <JobHeader/>
-        <div id='info-container'>
-          <InfoSection/>
-          <div id='side-bar-container'>
-            <Catalogue/>
-            <Copyright/>
-          </div>
-        </div>
-      </div>
-    </div>
+      <>  
+        <h1>sop</h1>
+      </>
   )
 }
-
-const NameHeader = () => (
-  <div id='name-header-container'>
-    <h1 id='firstname' className='vertical-text'>
-      <span>J</span>
-      <span>O</span>
-      <span>S</span>
-      <span>E</span>
-    </h1>
-    <h1 id='lastname' className='vertical-text'>
-      <span>R</span>
-      <span>I</span>
-      <span>0</span>
-      <span>S</span>
-    </h1>
-  </div>
-)
-
-const JobHeader = () => (
-  <div id="job-header-container">
-    <h1>Web Developer & </h1>
-    <h1>Software Engineer</h1>
-  </div>
-)
-
-const InfoSection = () => (
-  <div id="info-section-container">
-    <img src={bigScreenImage} alt="" />
-  </div>
-)
-
-const Catalogue = () => (
-  <div id="catalogue-container">
-    <div>01 about</div>
-    <div>02 projects</div>
-    <div>03 contact</div>
-  </div>
-)
-
-const Copyright = () => (
-  <div id="copyright-container">
-    Copyright and all that other stuff...
-  </div>
-)
 
 export default App

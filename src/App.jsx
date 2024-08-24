8c83e1f4fd7a949492ef2a44118155750ff7364f
Jose Rios <joserios0531@gmail.com>
2024-08-23 17:40:02 -0700

    Was able to get a nice background and the cursor chasing gradient bubble div down, this code may be subject to change

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,33 @@ import * as React from 'react';
 import './styles/index.scss'
 
 function App() {
+  const [mousePosition, setMousePosition] = React.useState({
+      x: 0,
+      y: 0,
+    }
+  );
+  console.log(mousePosition);
+
+  React.useEffect(() => {
+    const mouseMove = e => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY
+      })
+    }
+    window.addEventListener("mousemove", mouseMove)
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    }
+  }, []);
 
   return (
     <>
-      <div class="gradient-bubble">
-
-      </div>
+      <div style={{
+        position: 'fixed',
+        left: `${mousePosition.x - 200}px`,
+        top: `${mousePosition.y - 200}px`,
+      }} className="gradient-bubble"/>
     </>
   )
 }

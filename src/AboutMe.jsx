8be89fbaf4607c292765f1b762d9a280e30bac74
josedrios <joserios0531@gmail.com>
2025-02-11 12:03:44 -0800

    Added skills icon section, revised about me paragraph

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -1,12 +1,32 @@
-import * as React from 'react'
-import Info from './info.json'
+import * as React from "react";
+import Info from "./info.json";
+
+import { FaCss3, FaHtml5, FaReact, FaJava, FaPython, FaSass, FaNodeJs, FaGitAlt, FaJsSquare, FaNpm } from "react-icons/fa";
+import { SiMysql, SiCplusplus } from "react-icons/si";
 
 function AboutMe() {
-    return (
-        <div className='content-item' id='about-me-section'>
-            <h1 id='about-me' className='content-item-header'>About Me</h1>
-            <p className='content-item-body'>{Info.aboutme}</p>
-        </div>
-    )
+  return (
+    <div className="content-item" id="about-me-section">
+      <h1 id="about-me" className="content-item-header">
+        About Me
+      </h1>
+      <p className="content-item-body">{Info.aboutme}</p>
+      <h1 id="skills-header">Skills</h1>
+      <div id="skills-section">
+  <FaCss3 title="CSS3" />
+  <FaHtml5 title="HTML5" />
+  <FaReact title="React.js" />
+  <FaJsSquare title="JavaScript" />
+  <FaJava title="Java" />
+  <SiCplusplus title="C++" />
+  <FaPython title="Python" />
+  <FaSass title="SASS" />
+  <SiMysql title="MySQL" />
+  <FaNpm title="NPM" />
+  <FaNodeJs title="Node.js" />
+  <FaGitAlt title="Git" />
+</div>
+    </div>
+  );
 }
-export default AboutMe
+export default AboutMe;
